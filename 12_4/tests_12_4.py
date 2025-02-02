394d4ca6e874d62258600042de73ae091843eb83
LyovkinIlya <lyovkin2001@mail.ru>
2025-02-02 20:31:44 +0300

    02.02

---
+++ 12_4/tests_12_4.py
@@ -0,0 +1,40 @@
+import rt_with_exceptions
+import unittest
+import logging
+
+
+class RunnerTest(unittest.TestCase):
+
+    def test_walk(self):
+        try:
+            r1 = rt_with_exceptions.Runner(10, 20)
+            for i in range(10):
+                r1.walk()
+            self.assertEqual(r1.distance, 50)
+            logging.info(f'"test_walk" выполнен успешно {r1}')
+        except:
+            logging.warning('Неверная скорость для Runner', exc_info=True)
+
+    def test_run(self):
+        try:
+            r2 = rt_with_exceptions.Runner('R2', -10)
+            for i in range(10):
+                r2.run()
+            self.assertEqual(r2.distance, 100)
+            logging.info('"test_run" выполнен успешно')
+        except:
+            logging.warning("Неверный тип данных для объекта Runner", exc_info=True)
+            return 0
+
+    def test_challenge(self):
+        runner_1 = rt_with_exceptions.Runner('Run3')
+        runner_2 = rt_with_exceptions.Runner('Run4')
+        for i in range(10):
+            runner_1.walk()
+            runner_2.run()
+        self.assertNotEqual(runner_1.distance, runner_2.distance)
+
+
+logging.basicConfig(level=logging.INFO, filemode="w", filename='runner_tests.log', encoding='UTF-8',
+                    format="%(asctime)s | %(levelname)s | %(message)s")
+
