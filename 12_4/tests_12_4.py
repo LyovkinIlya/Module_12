07196cf308ac82bdbf932f98808b53178347540f
LyovkinIlya <lyovkin2001@mail.ru>
2025-02-02 20:33:40 +0300

    02.02

--- 12_4/tests_12_4.py
+++ 12_4/tests_12_4.py
@@ -26,14 +26,6 @@ class RunnerTest(unittest.TestCase):
             logging.warning("Неверный тип данных для объекта Runner", exc_info=True)
             return 0
 
-    def test_challenge(self):
-        runner_1 = rt_with_exceptions.Runner('Run3')
-        runner_2 = rt_with_exceptions.Runner('Run4')
-        for i in range(10):
-            runner_1.walk()
-            runner_2.run()
-        self.assertNotEqual(runner_1.distance, runner_2.distance)
-
 
 logging.basicConfig(level=logging.INFO, filemode="w", filename='runner_tests.log', encoding='UTF-8',
                     format="%(asctime)s | %(levelname)s | %(message)s")
